b430de088b88ee17896414f710c9d2ed38a120a6
Javohir Abdullayev <abdullaev_zhavohir@mail.ru>
2021-03-15 19:03:44 +0500

    Решена задача 2-2 #9

---
+++ Task 2-2.cpp
@@ -0,0 +1,44 @@
+﻿// Task 2-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#define _USE_MATH_DEFINES 
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+double f1(const double x, const double a);
+double f2(const double x, const double a);
+
+int main()
+
+{
+	const auto a = 2.8;
+	double x;
+	cout << "Vvedite peremennuyu x = ";
+	cin >> x;
+	if (x < 1.2)
+	{
+		cout << " y = " << f1(x, a);
+	}
+	else
+	{
+		cout << " y = " << f2(x, a);
+	}
+
+	return 0;
+}
+
+double f1(const double x, const double a)
+{
+	return a * x * x + 4;
+}
+
+
+double f2(const double x, const double a)
+{
+	return (a + 4 * x) * sqrt(pow(x, 2 * a));
+}
+
+
