b4daa5dd071a54284bb000d7736fb90c15e84853
Javohir Abdullayev <abdullaev_zhavohir@mail.ru>
2021-03-15 18:51:54 +0500

    Решена задача 1-2 #5

---
+++ Новая папка/Task 1-2.cpp
@@ -0,0 +1,21 @@
+﻿// Task 1-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+	const double Pi = 3.14;
+	double radius, volume, square;
+
+	cout << "Vvedite radius shara:\n";
+	cin >> radius;
+	volume = (4 / 3) * Pi * (radius * radius * radius);
+	square = Pi * (radius * radius);
+	cout << "Ob'yom shara = " << volume << " Ploshad' shara = " << square;
+
+}
